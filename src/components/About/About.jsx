cd6c26baee70177c5b1a6e03373a1bbf21880820
danicorderoo <soydcordero@gmail.com>
2022-11-29 21:27:12 -0400

    modified about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,7 +4,7 @@ import styles from "./About.module.css";
 export default function About(props) {
   return (
     <div className={styles.container}>
-      <h1>Holisss soy About</h1>
+      <h1>About</h1>
     </div>
   );
 }
